b6ba4f125b8324718194c1483314dc3a5eabe714
Aleksandr Ponomarenko <mcmegavolt@gmail.com>
2013-11-26 12:24:37 +0200

    get org repos and last year repo stats from GitHub API

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
 require 'slim'
+require 'net/http'
+require 'json'
+
+def org_repos
+	uri = URI("https://api.github.com/orgs/rails/repos")
+	json_data = Net::HTTP.get(uri)
+	org_repos = JSON.parse(json_data)
+end
+
+def last_year_activity repo
+	uri = URI("https://api.github.com/repos/rails/#{repo}/stats/commit_activity")
+	json_data = Net::HTTP.get(uri)
+	stats = JSON.parse(json_data)
+end
 
 get '/' do
   slim :index
