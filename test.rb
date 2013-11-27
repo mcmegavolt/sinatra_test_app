84bd53004d9f8a8b7cda835c59e9ed3529af5f44
Aleksandr Ponomarenko <mcmegavolt@gmail.com>
2013-11-27 13:39:43 +0200

    test update

--- test.rb
+++ test.rb
@@ -14,9 +14,14 @@ class MyTest < Minitest::Test
     Sinatra::Application
   end
 
-  def test_it_displays_page
+  def test_it_displays_index_page
     get '/'
     assert last_response.ok?
+  end
+
+  def test_last_year_activity_method_returns_array
+    assert_kind_of Array, app.send(:last_year_activity, ORG)
+    assert_kind_of Hash, app.send(:all_repos_year_activity)
   end
 
 
